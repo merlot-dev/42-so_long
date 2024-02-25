350cf1b8b77d9d044cda7716113feb77ebd61d67
Jose Luis Garcia <josegar@42barcelona.com>
2024-02-25 12:39:39 +0100

    merge so_long.h

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: josegar2 <josegar2@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 16:11:54 by josegar2          #+#    #+#             */
-/*   Updated: 2024/02/23 00:32:51 by josegar2         ###   ########.fr       */
+/*   Updated: 2024/02/25 12:39:05 by josegar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,25 @@
 # include "libft.h"
 # include "mlx.h"
 # include <stdlib.h>
+
+typedef struct s_mlx
+{
+	void	*mlx;
+	int		maxx;
+	int		maxy;
+	void	*win;
+	int		winx;
+	int		winy;
+	int		row;
+	int 	col;
+}			t_mlx;
+
+typedef struct	s_img {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_img;
 
 #endif
